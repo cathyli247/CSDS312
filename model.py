ac4be2455855a2bc0ec5921874a4c610a6a3c739
Shiqi <2412230983lsq@gmail.com>
2022-03-12 01:36:45 -0500

    test model

--- model.py
+++ model.py
@@ -6,11 +6,11 @@ import random as rn
 import pandas as pd
 from IPython.display import display
 import tensorflow as tf
-from keras import backend as K
 
 from keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, TensorBoard
 from keras.layers import Dense, Conv2D, Flatten, GlobalAveragePooling2D, Dropout, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
+from keras_radam import RAdam
 from sklearn.metrics import cohen_kappa_score
 
 IMG_WIDTH = 456
@@ -22,6 +22,8 @@ TEST_DF_PATH = './Data/test.csv'
 TRAIN_IMG_PATH = "./Data/train_images/"
 TEST_IMG_PATH = './Data/test_images/'
 SAVED_MODEL_NAME = 'effnet_modelB5.h5'
+
+tf.compat.v1.disable_eager_execution()
 
 seed = 1234
 rn.seed(seed)
@@ -141,115 +143,6 @@ val_generator = train_datagen.flow_from_dataframe(train_df,
                                                   class_mode='raw',
                                                   subset='validation')
 
-class RAdam(tf.keras.optimizers.Optimizer):
-    def __init__(self, lr=0.001, beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., weight_decay=0., amsgrad=False,
-                 total_steps=0, warmup_proportion=0.1, min_lr=0., **kwargs):
-        super(RAdam, self).__init__(**kwargs)
-        with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            self.lr = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-            self.weight_decay = K.variable(weight_decay, name='weight_decay')
-            self.total_steps = K.variable(total_steps, name='total_steps')
-            self.warmup_proportion = K.variable(warmup_proportion, name='warmup_proportion')
-            self.min_lr = K.variable(lr, name='min_lr')
-        if epsilon is None:
-            epsilon = K.epsilon()
-        self.epsilon = epsilon
-        self.initial_decay = decay
-        self.initial_weight_decay = weight_decay
-        self.initial_total_steps = total_steps
-        self.amsgrad = amsgrad
-
-    def get_updates(self, loss, params):
-        grads = self.get_gradients(loss, params)
-        self.updates = [K.update_add(self.iterations, 1)]
-
-        lr = self.lr
-
-        if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations, K.dtype(self.decay))))
-
-        t = K.cast(self.iterations, K.floatx()) + 1
-
-        if self.initial_total_steps > 0:
-            warmup_steps = self.total_steps * self.warmup_proportion
-            decay_steps = self.total_steps - warmup_steps
-            lr = K.switch(
-                t <= warmup_steps,
-                lr * (t / warmup_steps),
-                lr * (1.0 - K.minimum(t, decay_steps) / decay_steps),
-                )
-
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='m_' + str(i)) for (i, p) in enumerate(params)]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='v_' + str(i)) for (i, p) in enumerate(params)]
-
-        if self.amsgrad:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='vhat_' + str(i)) for (i, p) in enumerate(params)]
-        else:
-            vhats = [K.zeros(1, name='vhat_' + str(i)) for i in range(len(params))]
-
-        self.weights = [self.iterations] + ms + vs + vhats
-
-        beta_1_t = K.pow(self.beta_1, t)
-        beta_2_t = K.pow(self.beta_2, t)
-
-        sma_inf = 2.0 / (1.0 - self.beta_2) - 1.0
-        sma_t = sma_inf - 2.0 * t * beta_2_t / (1.0 - beta_2_t)
-
-        for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-
-            m_corr_t = m_t / (1.0 - beta_1_t)
-            if self.amsgrad:
-                vhat_t = K.maximum(vhat, v_t)
-                v_corr_t = K.sqrt(vhat_t / (1.0 - beta_2_t) + self.epsilon)
-                self.updates.append(K.update(vhat, vhat_t))
-            else:
-                v_corr_t = K.sqrt(v_t / (1.0 - beta_2_t) + self.epsilon)
-
-            r_t = K.sqrt((sma_t - 4.0) / (sma_inf - 4.0) *
-                         (sma_t - 2.0) / (sma_inf - 2.0) *
-                         sma_inf / sma_t)
-
-            p_t = K.switch(sma_t > 5, r_t * m_corr_t / v_corr_t, m_corr_t)
-
-            if self.initial_weight_decay > 0:
-                p_t += self.weight_decay * p
-
-            p_t = p - lr * p_t
-
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
-            new_p = p_t
-
-            # Apply constraints.
-            if getattr(p, 'constraint', None) is not None:
-                new_p = p.constraint(new_p)
-
-            self.updates.append(K.update(p, new_p))
-        return self.updates
-
-    def get_config(self):
-        config = {
-            'lr': float(K.get_value(self.lr)),
-            'beta_1': float(K.get_value(self.beta_1)),
-            'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
-            'weight_decay': float(K.get_value(self.weight_decay)),
-            'epsilon': self.epsilon,
-            'amsgrad': self.amsgrad,
-            'total_steps': float(K.get_value(self.total_steps)),
-            'warmup_proportion': float(K.get_value(self.warmup_proportion)),
-            'min_lr': float(K.get_value(self.min_lr)),
-        }
-        base_config = super(RAdam, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
 # effnet = tf.keras.applications.efficientnet.EfficientNetB0(weights="imagenet",
 #                                                            include_top=False,
 #                                                            input_shape=(IMG_WIDTH, IMG_HEIGHT, CHANNELS))
@@ -262,7 +155,7 @@ effnet = tf.keras.applications.efficientnet.EfficientNetB5(weights=None,
 #                         include_top=False,
 #                         input_shape=(IMG_WIDTH, IMG_HEIGHT, CHANNELS))
 
-effnet.load_weights('efficientnet-b5_imagenet_1000_notop.h5')
+# effnet.load_weights('efficientnet-b5_imagenet_1000_notop.h5')
 
 effnet.trainable = False
 
@@ -275,9 +168,9 @@ model = tf.keras.Sequential([
 ])
 
 model.compile(
-    loss='binary_crossentropy',
-    optimizer=RAdam(lr=0.01),
-    metrics=['binary_accuracy'])
+    loss='mse',
+    optimizer=RAdam(learning_rate=0.001),
+    metrics=['mse', 'acc'])
 
 checkpoint = tf.keras.callbacks.ModelCheckpoint(SAVED_MODEL_NAME, monitor='val_loss', save_best_only=True, mode='min')
 earlystop = EarlyStopping(patience=5, monitor='val_loss', min_delta=0.0005)
@@ -285,10 +178,10 @@ earlystop = EarlyStopping(patience=5, monitor='val_loss', min_delta=0.0005)
 model.summary()
 #
 history = model.fit(train_generator,
-                    batch_size=BATCH_SIZE,
+                    steps_per_epoch=train_generator.samples // BATCH_SIZE,
                     epochs=25,
                     validation_data=val_generator,
-                    validation_batch_size=BATCH_SIZE,
+                    validation_steps = val_generator.samples // BATCH_SIZE,
                     callbacks=[earlystop, checkpoint])
 
 model.load_weights(SAVED_MODEL_NAME)
